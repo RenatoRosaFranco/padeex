924f43394e8c3627df60e46c4799f71e8275b299
Renato Rosa Franco <renato_ny@live.com>
2026-03-01 15:54:28 -0600

    feature: fix migration problem.

--- db/migrate/20260227180000_backfill_tenant_padel.rb
+++ db/migrate/20260227180000_backfill_tenant_padel.rb
@@ -4,9 +4,11 @@ class BackfillTenantPadel < ActiveRecord::Migration[8.1]
   def up
     padel = Tenant.find_or_create_by!(slug: "padel") { |t| t.name = "Padel" }
 
-    [WaitlistEntry, InvestmentInterest, Post, User].each do |model|
+    [WaitlistEntry, InvestmentInterest, Post].each do |model|
       model.unscoped.where(tenant_id: nil).update_all(tenant_id: padel.id)
     end
+
+    execute("UPDATE users SET tenant_id = #{padel.id} WHERE tenant_id IS NULL")
   end
 
   def down
